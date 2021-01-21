6ee62a8b0a64e40919eab28877d6297f6eb03ccc
Ingrid Chen <ingrid831206@gmail.com>
2021-01-22 07:53:30 +0800

    learning lambda

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(require('./user/user_route'));
+
+module.exports = app;
